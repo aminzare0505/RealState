3e26e6bcdda3392d17d04ba9d6a6b0a78a23cbc6
a.zare <Aminzare0505@gmail.com>
2025-02-05 14:54:04 +0330

    Add project files.

---
+++ RealState/Controllers/CategoriesController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RealState.Models;
+
+namespace RealState.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private static List<Category> categories=new List<Category>() { 
+        new Category(){Id=1,Name="Name 1",ImageUrl="ImageUrl 1" },
+        new Category(){Id=2,Name="Name 2",ImageUrl="ImageUrl 2" }
+        };
+
+        [HttpGet("Get")]
+        public IEnumerable<Category> GET()
+        {
+            return categories;
+        }
+        [HttpPost("Insert")]
+        public void Insert([FromBody]Category category)
+        {
+            categories.Add(category);
+        }
+        [HttpPut("{id}")]
+        public void Put(int id,[FromBody] Category category)
+        {
+            categories[id]=category;
+        }
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            categories.RemoveAt(id              );
+        }
+    }
+}

55af4860a2af390472bb40c5b85b272b1c0a0f9d
a.zare <Aminzare0505@gmail.com>
2025-02-06 23:36:33 +0330

    migration

---
+++ RealState/Data/ApiDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RealState.Models;
+
+namespace RealState.Data
+{
+    public class ApiDBContext:DbContext
+    {
+        private const string _ConnectionString = "Server=.;Database=RealStateDb;Integrated Security=true;";
+        public DbSet<Category> Categories{ get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=.;Initial Catalog=RealStateDb;Integrated Security=True;Trust Server Certificate=True");
+        }
+    }
+}
---
+++ RealState/Migrations/20250206195201_ImageUrl2.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RealState.Migrations
+{
+    /// <inheritdoc />
+    public partial class ImageUrl2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImageUrl1",
+                table: "Categories",
+                newName: "ImageUrl2");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImageUrl2",
+                table: "Categories",
+                newName: "ImageUrl1");
+        }
+    }
+}
